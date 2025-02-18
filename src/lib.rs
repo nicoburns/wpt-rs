ca886e78fccd7f5ba1553ab2e8ab5b90800e17b4
Nico Burns <nico@nicoburns.com>
2025-02-18 18:33:40 +1300

    Use jemalloc as global allocator

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,8 @@ pub use report::{
     RunInfo, SubtestResult, SubtestStatus, TestResult, TestScore, TestStatus, WptReport, WptScores,
 };
 pub use score::{score_wpt_report, AreaScores};
+
+// Use jemalloc as the allocator
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
